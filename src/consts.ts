2f0514a51bea1a60a58d9d8bee94cca201092931
Yulong <yulong@example.com>
2026-05-21 10:59:30 +0200

    feat: init archive website

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Yulong Archive';
+export const SITE_DESCRIPTION = '大清帝国军机处档案馆|道光十六年';
